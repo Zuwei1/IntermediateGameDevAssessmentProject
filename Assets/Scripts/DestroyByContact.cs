97dd945d631a89a5ed82ba165faf06f13b22bdfd
Zuwei1 <50092539+Zuwei1@users.noreply.github.com>
2020-05-14 20:41:29 +1000

    base game

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameController gameController;
+
+	void Start() {
+		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+		if(gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+		if(gameController == null) {
+			Debug.Log("Cannot find game 'GameController' script");
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Boundry") {
+			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+		if(other.tag == "Player") {
+		Instantiate(playerExplosion, transform.position, transform.rotation);
+		gameController.GameOver();
+		}
+		gameController.AddScore(scoreValue);
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+		
+	}
+}
---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+{
+    // TODO: ATTACH THIS TO THE EXPLOSION ANIMATIONS
+    public float lifetime;
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+}
