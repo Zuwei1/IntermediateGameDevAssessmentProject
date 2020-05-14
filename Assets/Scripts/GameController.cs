37c4ee9d25ef6410de119b55c67ebfcd16cb5ec5
Zuwei1 <50092539+Zuwei1@users.noreply.github.com>
2020-05-14 23:01:51 +1000

    added stars and scrolling background

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,7 +11,7 @@ public class GameController : MonoBehaviour
     private bool restart;
     public Text scoreText;
     private int score;
-    public GameObject hazard;
+    public GameObject[] hazards;
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
@@ -37,6 +37,7 @@ public class GameController : MonoBehaviour
         yield return new WaitForSeconds(startWait);
         while(true) {
         for(int i = 0; i < hazardCount; i++) {
+        GameObject hazard = hazards[Random.Range(0,hazards.Length)];
         Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
         Quaternion spawnRotation = Quaternion.identity;
         Instantiate (hazard, spawnPosition, spawnRotation); 
